311aeeeb122902bd35f7055d39057b44bd270180
Lucas Schurer <lschurerm@gmail.com>
2022-10-14 15:09:48 -0300

    Create projectile events

---
+++ GeneticAlgorithmUnity/Assets/Scripts/Events/Projectiles/ProjectileEventController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles all the events of a specific entity
+/// Including method register/unregister and 
+/// event invokes
+/// </summary>
+namespace Game.Events
+{
+    public class ProjectileEventController : EventController<ProjectileEventType, ProjectileEventContext>
+    {
+    }
+}
+
+    }
+}
