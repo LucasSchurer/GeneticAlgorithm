f99397faa04e0f34c7cee2ba8ab8aad637c1285a
Lucas Schurer <lschurer@inf.ufsm.br>
2022-11-07 23:42:35 -0300

    Create TraitExecutionType enum

--- GeneticAlgorithmUnity/Assets/Scripts/Traits/Traits/Trait.cs
+++ GeneticAlgorithmUnity/Assets/Scripts/Traits/Traits/Trait.cs
@@ -5,9 +5,10 @@ using Game.Traits.Effects;
 namespace Game.Traits
 {
     public abstract class Trait<Type, Context> : ScriptableObject
-where Context : EventContext
+        where Context : EventContext
     {
         public TraitIdentifier identifier;
+        public TraitExecutionType executionType;
         public float cooldown;
         public Type eventType;
         public EventExecutionOrder executionOrder;
---
+++ GeneticAlgorithmUnity/Assets/Scripts/Traits/Traits/TraitExecutionType.cs
@@ -0,0 +1,8 @@
+namespace Game.Traits
+{
+    public enum TraitExecutionType
+    {
+        EventBased,
+        WhenAdded
+    } 
+}
