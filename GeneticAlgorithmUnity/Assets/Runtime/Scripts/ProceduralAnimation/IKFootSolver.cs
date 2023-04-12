d2001694657998adadf832e46bddc7de59c2da87
Lucas Schurer <lschurerm@gmail.com>
2023-04-12 15:51:32 -0300

    Add interpolation to target movement

--- GeneticAlgorithmUnity/Assets/Runtime/Scripts/ProceduralAnimation/IKFootSolver.cs
+++ GeneticAlgorithmUnity/Assets/Runtime/Scripts/ProceduralAnimation/IKFootSolver.cs
@@ -10,24 +10,85 @@ namespace Game.ProceduralAnimation
         private LayerMask _raycastLayer;
         [SerializeField]
         private float _maxRayDistance;
+        [SerializeField]
+        private Transform _raycastOrigin;
+        [SerializeField]
+        private Vector2 _maxDistanceRange;
+        private float _maxDistance;
+        [SerializeField]
+        private float _moveSmoothness;
 
-        private Transform _parentTransform;
-        private Vector3 _adjustedPosition;
+        private Vector3 _raycastOriginPosition;
+        private Vector3 _possiblePosition;
+
+        private bool _isMoving = false;
 
         private void Awake()
         {
-            _parentTransform = transform.parent.transform;
+            _raycastOriginPosition = _raycastOrigin.position;
+            _raycastOriginPosition.y += 1f;
+
+            if (GroundRaycast(_raycastOriginPosition, out Vector3 hitPosition))
+            {
+                transform.position = hitPosition;
+            }
+
+            _maxDistance = Random.Range(_maxDistanceRange.x, _maxDistanceRange.y);
         }
 
         private void Update()
         {
-            _adjustedPosition = transform.position;
-            _adjustedPosition.y = _parentTransform.position.y;
-
-            if (Physics.Raycast(_adjustedPosition, Vector3.down, out RaycastHit hit, _maxRayDistance, _raycastLayer))
+            if (!_isMoving)
             {
-                transform.position = hit.point;
+                if (GroundRaycast(_raycastOrigin.position, out Vector3 hitPosition))
+                {
+                    _possiblePosition = hitPosition;
+                }
+
+                if (Vector3.Distance(_possiblePosition, transform.position) >= _maxDistance)
+                {
+                    StartCoroutine(MoveCoroutine());
+                }
             }
+        }
+
+        private IEnumerator MoveCoroutine()
+        {
+            _isMoving = true;
+
+            Vector3 currentPosition = transform.position;
+            float timeElapsed = 0f;
+
+            while (timeElapsed <= _moveSmoothness)
+            {
+                transform.position = Vector3.Lerp(currentPosition, _possiblePosition, timeElapsed / _moveSmoothness);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.position = _possiblePosition;
+            _maxDistance = Random.Range(_maxDistanceRange.x, _maxDistanceRange.y);
+            _isMoving = false;
+
+            yield return null;
+        }
+
+        private bool GroundRaycast(Vector3 originPosition, out Vector3 hitPosition)
+        {
+            if (Physics.Raycast(originPosition, Vector3.down, out RaycastHit hit, _maxRayDistance, _raycastLayer))
+            {
+                hitPosition = hit.point;
+                return true;
+            } else
+            {
+                hitPosition = Vector3.zero;
+                return false;
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawSphere(_possiblePosition, 0.01f);
         }
     } 
 }
