fea45f3519dfcc84336cd450d5f9c25efe79c07d
Lucas Schurer <lschurer@inf.ufsm.br>
2023-04-17 00:12:18 -0300

    Add crosshair hit on player hit dealt

--- GeneticAlgorithmUnity/Assets/Runtime/Scripts/Entities/Player/Crosshair.cs
+++ GeneticAlgorithmUnity/Assets/Runtime/Scripts/Entities/Player/Crosshair.cs
@@ -1,13 +1,17 @@
+using Game.Events;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Game.Entities
 {
-    public class Crosshair : MonoBehaviour
+    public class Crosshair : MonoBehaviour, IEventListener
     {
         [SerializeField]
         private float _hitIndicatorTime = 1f;
+        [SerializeField]
+        private EntityEventController _playerEventController;
+
         private float _realHitIndicatorTime = 1f;
         private Coroutine _hitCoroutine;
 
@@ -22,15 +26,7 @@ namespace Game.Entities
             _hitIndicatorTransparency = _crosshairMaterial.GetFloat(HitTransparency);
         }
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-            {
-                Hit();
-            }
-        }
-
-        private void Hit()
+        private void OnHitDealt(ref EntityEventContext ctx)
         {
             if (_hitCoroutine == null)
             {
@@ -62,6 +58,32 @@ namespace Game.Entities
             _crosshairMaterial.SetFloat(HitTransparency, _hitIndicatorTransparency);
             
             _hitCoroutine = null;
+        }
+
+        private void OnEnable()
+        {
+            StartListening();
+        }
+
+        private void OnDisable()
+        {
+            StopListening();   
+        }
+
+        public void StartListening()
+        {
+            if (_playerEventController)
+            {
+                _playerEventController.AddListener(EntityEventType.OnHitDealt, OnHitDealt, EventExecutionOrder.After);
+            }
+        }
+
+        public void StopListening()
+        {
+            if (_playerEventController)
+            {
+                _playerEventController.RemoveListener(EntityEventType.OnHitDealt, OnHitDealt, EventExecutionOrder.After);
+            }
         }
     } 
 }
