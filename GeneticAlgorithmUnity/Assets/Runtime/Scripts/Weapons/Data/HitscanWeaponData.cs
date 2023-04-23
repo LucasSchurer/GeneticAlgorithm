32845ed3885c905f1e5f9d566b109be7fe8b60f5
Lucas Schurer <lschurer@inf.ufsm.br>
2023-04-23 01:36:42 -0300

    Add particle data

--- GeneticAlgorithmUnity/Assets/Runtime/Scripts/Weapons/Data/HitscanWeaponData.cs
+++ GeneticAlgorithmUnity/Assets/Runtime/Scripts/Weapons/Data/HitscanWeaponData.cs
@@ -7,9 +7,18 @@ namespace Game.Weapons
     [CreateAssetMenu(menuName = Constants.WeaponDataMenuName + "/HitscanWeaponData")]
     public class HitscanWeaponData : WeaponData
     {
+        [Header("General Settings")]
         [SerializeField]
         private float _hitRange;
 
+        [Header("Graphical")]
+        [SerializeField]
+        private ParticleSystem _attackParticle;
+        [SerializeField]
+        private ParticleSystem _hitParticle;
+
         public float HitRange => _hitRange;
+        public ParticleSystem AttackParticle => _attackParticle;
+        public ParticleSystem OnHitParticle => _hitParticle;
     }
 }
